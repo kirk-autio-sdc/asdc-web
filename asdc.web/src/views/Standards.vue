19b1664de2bbebf0d755d6edcd3bcb3cd0829f98
Kirk Autio <kirk.autio@gov.ab.ca>
2022-01-23 14:15:58 -0700

    directory correction

---
+++ asdc.web/src/views/Standards.vue
@@ -0,0 +1,11 @@
+﻿<template>
+  <div class="standards"></div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Standards extends Vue {}
+</script>
+
+<style scoped></style>
